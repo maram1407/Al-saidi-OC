c083a792e26decd01919add9db933a0b4fa505c2
aimanydammag <aimanydammag@outlook.com>
2020-06-02 00:02:03 -0800

    1

--- lab 2 OC/src/Process.java
+++ lab 2 OC/src/Process.java
@@ -1,79 +1,43 @@
 import java.util.ArrayList;
 import java.util.Random;
-
 public class Process {
-	private int id;
-	private int quant_Process;
-	private int time;
-	private int timeThread;
-	private int Count;
-	private ArrayList<Thread> threads;
-	Random randomTime = new Random ();
-	Random randomCount = new Random ();
+	Random rand = new Random();
+
+	private ArrayList<Thread> Threads;
+
+	private String description = "Процесс ";
+	public int maxTime;
+	public int currentTime;		
+	private int threadMaxTime;
 	
-	public Process (int id, int quant_Process, int Count) {
-		this.id = id;
-		this.Count = Count;
-		this.quant_Process = quant_Process;
-		this.time = quant_Process;
-		this.timeThread = quant_Process;
-		threads = new ArrayList<Thread>();
-		for(int i = 1; i < Count; i ++) {
-			if (timeThread != 0)
-			threads.add(new Thread(i, --timeThread));
+	
+	public Process(String description, int maxTime, Priority priority) {
+
+		this.description += description + " с приоритетом: " + priority.toString();		
+		this.maxTime = maxTime * priority.getNum();		
+		Threads = new ArrayList<Thread>();
+		int threadsNumber = rand.nextInt(4) + 1;		
+		threadMaxTime = this.maxTime/threadsNumber;
+
+		for (int i = 0; i < threadsNumber; i++) {
+			Threads.add(new Thread("" + i, rand.nextInt(10) + 1, threadMaxTime));
 		}
 	}
 	
-	public String getId() {
-		return "Процесс" + id;
-	}
-	
-	public int getQuant() {
-		return quant_Process;
-	}
-	
-	public Thread getThread()
-    {
-        if (threads.size() == 0) 
-        	return null;
-        return threads.get(0);
-    }
-	
-	public int reduceTime() {
-		--time;
-		return time;
-	}
-	
-	public boolean restore() {
-		if(threads.size() != 0 && getTime() == 0 && getThread().getQuant() > 0) {
-			time = quant_Process;
-			return true;
-		}
-		return false;
-	}
-	
-	public int getTime() {
-		return time;
-	}
-	
-	public void run() {
-		if (threads.size() != 0)
-			System.out.println(getThread().printWork());
-		if (threads.size() != 0 && getThread().getQuant() >= 0 ) {
-			getThread().reduceQuant();
-			reduceTime();
-			if (getThread().getQuant() == 0) {
-				System.out.println("Поток " + id + " выполнен");
-				threads.remove(0);
-			}
-				
-		}
+	public int getCount() {
+		return Threads.size();
 	}
 
-	public int Size() {
-		if (threads.size() == 0) 
-			return 0;
-		return threads.size();
+	public boolean isEmpty() {
+		return !Threads.size() >0;
+	}	
+
+	public String getDescription() {
+		return description;
 	}
 
+	public boolean HaveTime() {
+		return maxTime > currentTime;
+	}
+	
 }
--- lab 2 OC/src/Thread.java
+++ lab 2 OC/src/Thread.java
@@ -1,26 +1,44 @@
 
-public class Thread {private int id = 0;
-private int quant_Thread = 0;
-private int work;
+public class Thread {private String description = "Поток ";
+private int maxTime;
+private int currentTime = 0;
+private int requiredTime;
 
-public Thread (int id, int quant_Thread) {
-	this.id = id;
-	this.quant_Thread = quant_Thread;
+public Thread(String description, int requiredTime, int maxTime) {
+	this.description += description;
+	this.maxTime = maxTime;
+	this.requiredTime = requiredTime;
+}
+public int getRequiredTime() {
+	return requiredTime;
 }
 
-public int getQuant() {
-	return quant_Thread;
+public int getCurrentTime() {
+	return currentTime;
 }
 
-public int printWork() {
-	work++;
-	return work;
+public int getMaxTime() {
+	return maxTime;
 }
 
-public int reduceQuant() {
-	--quant_Thread;
-	return quant_Thread;
+public String getDescription() {
+	return description;
 }
 
+public void executeThread() {
+	currentTime++;
+	System.out.println(getInfo());
+}	
+
+public boolean needTime() {
+	return requiredTime > currentTime;
+}
+
+public boolean haveTime() {
+	return maxTime > currentTime;
+}
+public String getInfo() {
+	return description + " maxTime:" + maxTime + " currentTime:" + currentTime + " requiredTime:" + requiredTime;
+}
 
 }
